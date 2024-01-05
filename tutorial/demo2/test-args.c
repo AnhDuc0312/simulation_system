6f3f647073e8cfa8ddbcc8955c93be8c180fe011
AnhDuc0312 <duc.leanh0312@gmail.com>
2024-01-05 23:07:04 +0700

    add data

---
+++ tutorial/demo2/test-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	printf("Number of arguments: %d\n", argc);
+	for (i = 0; i < argc; i++)
+		printf("\targv[%d] = %s\n", i, argv[i]);
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
